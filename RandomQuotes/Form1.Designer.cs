6160f1f39eb012a860f3fdaf6cd159ca8e46c0a6
Jamie Matthews <jamiematthews48@hotmail.co.uk>
2022-07-09 22:10:27 +0100

    Added main functionality

--- RandomQuotes/Form1.Designer.cs
+++ RandomQuotes/Form1.Designer.cs
@@ -29,6 +29,8 @@
         private void InitializeComponent()
         {
             this.btnShuffle = new System.Windows.Forms.Button();
+            this.lbQuote = new System.Windows.Forms.Label();
+            this.lbAuthor = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnShuffle
@@ -41,21 +43,48 @@
             this.btnShuffle.UseVisualStyleBackColor = true;
             this.btnShuffle.Click += new System.EventHandler(this.btnShuffle_Click);
             // 
+            // lbQuote
+            // 
+            this.lbQuote.AutoSize = true;
+            this.lbQuote.Font = new System.Drawing.Font("Roboto", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbQuote.Location = new System.Drawing.Point(186, 162);
+            this.lbQuote.Name = "lbQuote";
+            this.lbQuote.Size = new System.Drawing.Size(715, 116);
+            this.lbQuote.TabIndex = 1;
+            this.lbQuote.Text = "THE QUOTE WILL GO HERE AND\r\nIT WILL BE EXCELLENT!";
+            this.lbQuote.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbAuthor
+            // 
+            this.lbAuthor.AutoSize = true;
+            this.lbAuthor.Font = new System.Drawing.Font("Roboto", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbAuthor.Location = new System.Drawing.Point(728, 297);
+            this.lbAuthor.Name = "lbAuthor";
+            this.lbAuthor.Size = new System.Drawing.Size(173, 33);
+            this.lbAuthor.TabIndex = 2;
+            this.lbAuthor.Text = "- Author Here";
+            this.lbAuthor.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
             // frm1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1084, 561);
+            this.Controls.Add(this.lbAuthor);
+            this.Controls.Add(this.lbQuote);
             this.Controls.Add(this.btnShuffle);
             this.Name = "frm1";
             this.Text = "Random Quotes";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button btnShuffle;
+        private System.Windows.Forms.Label lbQuote;
+        private System.Windows.Forms.Label lbAuthor;
     }
 }
 
--- RandomQuotes/Form1.cs
+++ RandomQuotes/Form1.cs
@@ -17,22 +17,26 @@ namespace RandomQuotes
         public frm1()
         {
             InitializeComponent();
+            if (!File.Exists("quotes.txt"))
+            {
+                WebClient Client = new WebClient();
+                Client.DownloadFile("https://link.storjshare.io/jxu7x3q2w22ls6lq7x3diqrozlka/publicresources%2FQuotesRandom.txt?download=1", "quotes.txt");
+                Client.DownloadFile("https://link.storjshare.io/jwiechv4ycd2ysoxiufzfmdfldya/publicresources%2Fbackgrounds%2F1.jpg?download=1", "1.jpg");
+                Client.DownloadFile("https://link.storjshare.io/jvref5prl4mb4johwa4xik6i2f4q/publicresources%2Fbackgrounds%2F2.jpg?download=1", "2.jpg");
+                Client.DownloadFile("https://link.storjshare.io/jvge35wr2r4pc2kgtaiaiizjd25q/publicresources%2Fbackgrounds%2F3.jpg?download=1", "3.jpg");
+                Client.DownloadFile("https://link.storjshare.io/jxhcai4ktnczukynsezlkufzdcdq/publicresources%2Fbackgrounds%2F4.jpg?download=1", "4.jpg");
+                Client.DownloadFile("https://link.storjshare.io/jvdp6neovneonhu2pc6zu32de6qq/publicresources%2Fbackgrounds%2F5.jpg?download=1", "5.jpg");
+                Client.DownloadFile("https://link.storjshare.io/jvfvq3nhdta2rgc5vduk3x3nvdsq/publicresources%2Fbackgrounds%2F6.jpg?download=1", "6.jpg");
+                Client.DownloadFile("https://link.storjshare.io/jvncubbznwmej5zhxwz2lpezgwwa/publicresources%2Fbackgrounds%2F7.jpg?download=1", "7.jpg");
+
+
+            }
         }
 
         private void btnShuffle_Click(object sender, EventArgs e)
         {
-            WebClient Client = new WebClient();
-            Client.DownloadFile("https://link.storjshare.io/jxu7x3q2w22ls6lq7x3diqrozlka/publicresources%2FQuotesRandom.txt?download=1", "quotes.txt");
-            Client.DownloadFile("https://link.storjshare.io/jwiechv4ycd2ysoxiufzfmdfldya/publicresources%2Fbackgrounds%2F1.jpg?download=1", "1.jpg");
-            Client.DownloadFile("https://link.storjshare.io/jvref5prl4mb4johwa4xik6i2f4q/publicresources%2Fbackgrounds%2F2.jpg?download=1", "2.jpg");
-            Client.DownloadFile("https://link.storjshare.io/jvge35wr2r4pc2kgtaiaiizjd25q/publicresources%2Fbackgrounds%2F3.jpg?download=1", "3.jpg");
-            Client.DownloadFile("https://link.storjshare.io/jxhcai4ktnczukynsezlkufzdcdq/publicresources%2Fbackgrounds%2F4.jpg?download=1", "4.jpg");
-            Client.DownloadFile("https://link.storjshare.io/jvdp6neovneonhu2pc6zu32de6qq/publicresources%2Fbackgrounds%2F5.jpg?download=1", "5.jpg");
-            Client.DownloadFile("https://link.storjshare.io/jvfvq3nhdta2rgc5vduk3x3nvdsq/publicresources%2Fbackgrounds%2F6.jpg?download=1", "6.jpg");
-            Client.DownloadFile("https://link.storjshare.io/jvncubbznwmej5zhxwz2lpezgwwa/publicresources%2Fbackgrounds%2F7.jpg?download=1", "7.jpg");
-
             //Initialise quotes string array
-            string[] quotes = new string[new System.IO.FileInfo("quotes.txt").Length];
+            string[] quotes = new string[File.ReadAllLines("quotes.txt").Count()];
 
             //Read in quotes from file
             using (StreamReader sr = new StreamReader("quotes.txt"))
@@ -53,22 +57,9 @@ namespace RandomQuotes
             //Set background image of form
             this.BackgroundImage = Image.FromFile("" + rnd2 + ".jpg");
 
-            foreach (var element in quotes)
-            {
-                var split = element.Split('~');
-                /* Usage:
-                 * split[0] is for the quote text
-                 * split[1] is for the quotes author
-                 */
-            }
-
-            //Search for required quote by cycling through them all
-            bool found = false;
-            int i = 0;
-            do
-            {
-                if (quotes[i])
-            } while (found == false);
+            string QuoteAndAuthor = quotes[rnd1];
+            lbQuote.Text = QuoteAndAuthor.Split('~')[0];
+            lbAuthor.Text = QuoteAndAuthor.Split('~')[1];
         }
     }
 }
